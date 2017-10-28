68924b795bba2d28c3a9d8830c3a73bf78613f5f
Oliver <rift95@live.se>
2017-10-28 15:26:16 +0200

    ..

--- test_target/demo.ts
+++
@@ -1,5 +0,0 @@
-import { Math } from './src/index';
-
-const adder = new Math();
-
-console.log(adder.add(2, 3));
--- test_target/src/index.ts
+++
@@ -1,5 +0,0 @@
-export class Math {
-    add(x: number, y: number): number {
-        return x + y;
-    }
-}
